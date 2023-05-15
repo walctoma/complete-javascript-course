fea68bafb9652ae14abb6257f5d3046ea00fa580
Tomasz Walczak <131537906+walctoma@users.noreply.github.com>
2023-05-15 17:52:41 +0200

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Losowanie liczby 1-20
+const number = Math.trunc(Math.random() * 20) + 1;
+// Przypisanie liczby do pola 
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  document.querySelector('.message').textContent = 'Piękna Liczba Stary! ✅';
+  console.log(guess, typeof guess);
+
+
+ // Warunek gdy liczba nie jest wpisana: 
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Brak Cyferki o_o';
+  }
+});
